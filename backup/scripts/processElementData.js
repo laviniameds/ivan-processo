17b40c803af8d0455419ba098f836c4169cc273b
Lavinia Medeiros <laviniameds@icloud.com>
2019-02-24 20:51:05 -0300

    renomeação arquivos

---
+++ backup/scripts/processElementData.js
@@ -0,0 +1,72 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_a7R50fstEei0J5mg9rEWvA,_nvyLUP9EEeiNf8InBZM3SA,_8XDKQf9EEeiNf8InBZM3SAActivity"]="./../../desenvolvimento_de_testes/deliveryprocesses/resources/Concepção da Feature_EDB46BAF_b6d04e91_Activity.jpeg"
+contentPage.processPage.imageFiles["_a7R50fstEei0J5mg9rEWvA,_nvyLUP9EEeiNf8InBZM3SA,_8XDKQf9EEeiNf8InBZM3SAActivityDetail"]="./../../desenvolvimento_de_testes/deliveryprocesses/resources/Concepção da Feature_EDB46BAF_b6d04e91_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_a7R50fstEei0J5mg9rEWvAActivity"]="./../../desenvolvimento_de_testes/deliveryprocesses/resources/Desenvolvimento de Testes_4ED32D11_4ed32d11_Activity.jpeg"
+contentPage.processPage.imageFiles["_a7R50fstEei0J5mg9rEWvA,_4uw08P9EEeiNf8InBZM3SA,_TCJpMf9FEeiNf8InBZM3SAActivity"]="./../../desenvolvimento_de_testes/deliveryprocesses/resources/Construção da Funcionalidade_AC7BA62_c4b4d060_Activity.jpeg"
+contentPage.processPage.imageFiles["_a7R50fstEei0J5mg9rEWvA,_4uw08P9EEeiNf8InBZM3SA,_TCJpMf9FEeiNf8InBZM3SAActivityDetail"]="./../../desenvolvimento_de_testes/deliveryprocesses/resources/Construção da Funcionalidade_AC7BA62_c4b4d060_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_a7R50fstEei0J5mg9rEWvA,_nvyLUP9EEeiNf8InBZM3SA,_p_Hwsf9EEeiNf8InBZM3SAActivity"]="./../../desenvolvimento_de_testes/deliveryprocesses/resources/Preparação do Ambiente_C65423C4_8f7006a6_Activity.jpeg"
+contentPage.processPage.imageFiles["_a7R50fstEei0J5mg9rEWvA,_wDM9kf9FEeiNf8InBZM3SA,_zQeK0P9FEeiNf8InBZM3SAActivity"]="./../../desenvolvimento_de_testes/deliveryprocesses/resources/Relatório do Teste_3450A491_4c346a32_Activity.jpeg"
+contentPage.processPage.imageFiles["_a7R50fstEei0J5mg9rEWvA,_wDM9kf9FEeiNf8InBZM3SA,_zQeK0P9FEeiNf8InBZM3SAActivityDetail"]="./../../desenvolvimento_de_testes/deliveryprocesses/resources/Relatório do Teste_3450A491_4c346a32_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_a7R50fstEei0J5mg9rEWvA,_nvyLUP9EEeiNf8InBZM3SAActivity"]="./../../desenvolvimento_de_testes/deliveryprocesses/resources/Preparação_ADB0C629_8869e8e_Activity.jpeg"
+contentPage.processPage.imageFiles["_a7R50fstEei0J5mg9rEWvA,_4uw08P9EEeiNf8InBZM3SAActivity"]="./../../desenvolvimento_de_testes/deliveryprocesses/resources/Construção_945E8D45_ef3465aa_Activity.jpeg"
+contentPage.processPage.imageFiles["_a7R50fstEei0J5mg9rEWvA,_4uw08P9EEeiNf8InBZM3SA,_UrQVAf9FEeiNf8InBZM3SAActivity"]="./../../desenvolvimento_de_testes/deliveryprocesses/resources/Teste da Feature_1CAADD3B_d697f339_Activity.jpeg"
+contentPage.processPage.imageFiles["_a7R50fstEei0J5mg9rEWvA,_4uw08P9EEeiNf8InBZM3SA,_UrQVAf9FEeiNf8InBZM3SAActivityDetail"]="./../../desenvolvimento_de_testes/deliveryprocesses/resources/Teste da Feature_1CAADD3B_d697f339_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_a7R50fstEei0J5mg9rEWvA,_wDM9kf9FEeiNf8InBZM3SAActivity"]="./../../desenvolvimento_de_testes/deliveryprocesses/resources/Documentação_C0C35E8_66e20e4d_Activity.jpeg"
+contentPage.processPage.elementUrls["_8XDKQf9EEeiNf8InBZM3SA"] = ["Concepção da Feature","desenvolvimento_de_testes/deliveryprocesses/Concepção da Feature_EDB46BAF.html"];
+contentPage.processPage.elementUrls["_mOCPQP9FEeiNf8InBZM3SA"] = ["Identificação de Cenário","desenvolvimento_de_testes/tasks/identificacao_de_cenario_98EB749E.html"];
+contentPage.processPage.elementUrls["_qsUYcP9FEeiNf8InBZM3SA"] = ["Identificar Erros","desenvolvimento_de_testes/tasks/identificar_erros_F6413FF1.html"];
+contentPage.processPage.elementUrls["_oHQatP9FEeiNf8InBZM3SA"] = ["Relatório Final de Testes","desenvolvimento_de_testes/workproducts/relatorio_final_de_testes_DA9FFF74.html"];
+contentPage.processPage.elementUrls["_wcHpUP9EEeiNf8InBZM3SA"] = ["Instalar Ferramentas de Teste","desenvolvimento_de_testes/tasks/instalar_ferramentas_de_teste_1D69FDB6.html"];
+contentPage.processPage.elementUrls["_wcQzQP9EEeiNf8InBZM3SA"] = ["Ambiente preparado para o desenvolvimento","desenvolvimento_de_testes/workproducts/ambiente_preparado_para_o_desenvolvimento_4E638D7.html"];
+contentPage.processPage.elementUrls["_qZmQw_9FEeiNf8InBZM3SA"] = ["Feature de Teste Completa","desenvolvimento_de_testes/workproducts/feature_de_teste_completa_55C222E7.html"];
+contentPage.processPage.elementUrls["_lS8gAf9FEeiNf8InBZM3SA"] = ["Desenvolvedor de Testes","desenvolvimento_de_testes/roles/desenvolvedor_de_testes_E71F026A.html"];
+contentPage.processPage.elementUrls["_0_TrNP9FEeiNf8InBZM3SA"] = ["Ambiente preparado para o desenvolvimento","desenvolvimento_de_testes/workproducts/ambiente_preparado_para_o_desenvolvimento_4E638D7.html"];
+contentPage.processPage.elementUrls["_lTDNsf9FEeiNf8InBZM3SA"] = ["Relatório Final de Testes","desenvolvimento_de_testes/workproducts/relatorio_final_de_testes_DA9FFF74.html"];
+contentPage.processPage.elementUrls["_lS8gAP9FEeiNf8InBZM3SA"] = ["Identificação de Funcionalidade","desenvolvimento_de_testes/tasks/identificacao_de_funcionalidade_B13A4524.html"];
+contentPage.processPage.elementUrls["_lTDNtP9FEeiNf8InBZM3SA"] = ["Feature de Teste Finalizada","desenvolvimento_de_testes/workproducts/feature_de_teste_finalizada_BB78DDDE.html"];
+contentPage.processPage.elementUrls["_oHIe4f9FEeiNf8InBZM3SA"] = ["Desenvolvedor de Testes","desenvolvimento_de_testes/roles/desenvolvedor_de_testes_E71F026A.html"];
+contentPage.processPage.elementUrls["_qZne5f9FEeiNf8InBZM3SA"] = ["Relatório Final de Testes","desenvolvimento_de_testes/workproducts/relatorio_final_de_testes_DA9FFF74.html"];
+contentPage.processPage.elementUrls["_oHIe4P9FEeiNf8InBZM3SA"] = ["Implementação da funcionalidade","desenvolvimento_de_testes/tasks/implementacao_da_funcionalidade_EE9D44F8.html"];
+contentPage.processPage.elementUrls["_0-8e0v9FEeiNf8InBZM3SA"] = ["Relatório Final de Testes","desenvolvimento_de_testes/workproducts/relatorio_final_de_testes_DA9FFF74.html"];
+contentPage.processPage.elementUrls["_nvyLUP9EEeiNf8InBZM3SA"] = ["Preparação","desenvolvimento_de_testes/deliveryprocesses/Preparação_ADB0C629.html"];
+contentPage.processPage.elementUrls["_q-efcP9FEeiNf8InBZM3SA"] = ["Consertar Erros","desenvolvimento_de_testes/tasks/consertar_erros_2775DF11.html"];
+contentPage.processPage.elementUrls["_a7R50fstEei0J5mg9rEWvA"] = ["Desenvolvimento de Testes","desenvolvimento_de_testes/deliveryprocesses/Desenvolvimento de Testes_4ED32D11.html"];
+contentPage.processPage.elementUrls["_0-5bgP9FEeiNf8InBZM3SA"] = ["Desenvolvedor de Testes","desenvolvimento_de_testes/roles/desenvolvedor_de_testes_E71F026A.html"];
+contentPage.processPage.elementUrls["_lTCmov9FEeiNf8InBZM3SA"] = ["Feature de Teste Completa","desenvolvimento_de_testes/workproducts/feature_de_teste_completa_55C222E7.html"];
+contentPage.processPage.elementUrls["_qZm30v9FEeiNf8InBZM3SA"] = ["Feature de Teste Incompleta","desenvolvimento_de_testes/workproducts/feature_de_teste_incompleta_31CD8EC8.html"];
+contentPage.processPage.elementUrls["_1aM3cf9FEeiNf8InBZM3SA"] = ["Coordenador dos Testes","desenvolvimento_de_testes/roles/coordenador_dos_testes_7D92BADA.html"];
+contentPage.processPage.elementUrls["_miyXQP9FEeiNf8InBZM3SA"] = ["Identificação de Papéis","desenvolvimento_de_testes/tasks/identificacao_de_papeis_453CED8D.html"];
+contentPage.processPage.elementUrls["_oHIe4_9FEeiNf8InBZM3SA"] = ["Feature de Teste Incompleta","desenvolvimento_de_testes/workproducts/feature_de_teste_incompleta_31CD8EC8.html"];
+contentPage.processPage.elementUrls["_od7OQP9FEeiNf8InBZM3SA"] = ["Implementação de Cenários","desenvolvimento_de_testes/tasks/implementao_de_cenarios_B86F1921.html"];
+contentPage.processPage.elementUrls["_qZmQwP9FEeiNf8InBZM3SA"] = ["Utilizar ferramenta automatizada","desenvolvimento_de_testes/tasks/utilizar_ferramenta_automatizada_CE119A15.html"];
+contentPage.processPage.elementUrls["_oHQasf9FEeiNf8InBZM3SA"] = ["Ambiente preparado para o desenvolvimento","desenvolvimento_de_testes/workproducts/ambiente_preparado_para_o_desenvolvimento_4E638D7.html"];
+contentPage.processPage.elementUrls["_4uw08P9EEeiNf8InBZM3SA"] = ["Construção","desenvolvimento_de_testes/deliveryprocesses/Construção_945E8D45.html"];
+contentPage.processPage.elementUrls["_UrQVAf9FEeiNf8InBZM3SA"] = ["Teste da Feature","desenvolvimento_de_testes/deliveryprocesses/Teste da Feature_1CAADD3B.html"];
+contentPage.processPage.elementUrls["_0-4NYP9FEeiNf8InBZM3SA"] = ["Documentar Erros","desenvolvimento_de_testes/tasks/documentar_erros_200668E5.html"];
+contentPage.processPage.elementUrls["_0_TrMf9FEeiNf8InBZM3SA"] = ["Feature de Teste Completa","desenvolvimento_de_testes/workproducts/feature_de_teste_completa_55C222E7.html"];
+contentPage.processPage.elementUrls["_oHQat_9FEeiNf8InBZM3SA"] = ["Feature de Teste Finalizada","desenvolvimento_de_testes/workproducts/feature_de_teste_finalizada_BB78DDDE.html"];
+contentPage.processPage.elementUrls["_lS9HEP9FEeiNf8InBZM3SA"] = ["Feature de Teste Incompleta","desenvolvimento_de_testes/workproducts/feature_de_teste_incompleta_31CD8EC8.html"];
+contentPage.processPage.elementUrls["_p_Hwsf9EEeiNf8InBZM3SA"] = ["Preparação do Ambiente","desenvolvimento_de_testes/deliveryprocesses/Preparação do Ambiente_C65423C4.html"];
+contentPage.processPage.elementUrls["_zQeK0P9FEeiNf8InBZM3SA"] = ["Relatório do Teste","desenvolvimento_de_testes/deliveryprocesses/Relatório do Teste_3450A491.html"];
+contentPage.processPage.elementUrls["_qZne4v9FEeiNf8InBZM3SA"] = ["Ambiente preparado para o desenvolvimento","desenvolvimento_de_testes/workproducts/ambiente_preparado_para_o_desenvolvimento_4E638D7.html"];
+contentPage.processPage.elementUrls["_oHPMkv9FEeiNf8InBZM3SA"] = ["Feature de Teste Completa","desenvolvimento_de_testes/workproducts/feature_de_teste_completa_55C222E7.html"];
+contentPage.processPage.elementUrls["_0_TEIv9FEeiNf8InBZM3SA"] = ["Feature de Teste Incompleta","desenvolvimento_de_testes/workproducts/feature_de_teste_incompleta_31CD8EC8.html"];
+contentPage.processPage.elementUrls["_1aM3cP9FEeiNf8InBZM3SA"] = ["Revisar Relatório Final","desenvolvimento_de_testes/tasks/revisar_relatorio_final_1DC84E9C.html"];
+contentPage.processPage.elementUrls["_wDM9kf9FEeiNf8InBZM3SA"] = ["Documentação","desenvolvimento_de_testes/deliveryprocesses/Documentação_C0C35E8.html"];
+contentPage.processPage.elementUrls["_qZmQwf9FEeiNf8InBZM3SA"] = ["Desenvolvedor de Testes","desenvolvimento_de_testes/roles/desenvolvedor_de_testes_E71F026A.html"];
+contentPage.processPage.elementUrls["_qZne6P9FEeiNf8InBZM3SA"] = ["Feature de Teste Finalizada","desenvolvimento_de_testes/workproducts/feature_de_teste_finalizada_BB78DDDE.html"];
+contentPage.processPage.elementUrls["_lTCmpf9FEeiNf8InBZM3SA"] = ["Ambiente preparado para o desenvolvimento","desenvolvimento_de_testes/workproducts/ambiente_preparado_para_o_desenvolvimento_4E638D7.html"];
+contentPage.processPage.elementUrls["_0-8e0P9FEeiNf8InBZM3SA"] = ["Feature de Teste Finalizada","desenvolvimento_de_testes/workproducts/feature_de_teste_finalizada_BB78DDDE.html"];
+contentPage.processPage.elementUrls["_TCJpMf9FEeiNf8InBZM3SA"] = ["Construção da Funcionalidade","desenvolvimento_de_testes/deliveryprocesses/Construção da Funcionalidade_AC7BA62.html"];
